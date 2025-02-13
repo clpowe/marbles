51c1fa022f9095e32579add5298b92ff3c194cac
clpowe <clpowe@gmail.com>
2025-02-13 06:29:54 -0500

    index update

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,76 +1,77 @@
 <script setup>
-import { useChildrenStore } from "~/store/childrenStore";
-const { children: c, subscribeToSSE } = useChildrenStore();
+	let children = useState('message', () => [])
 
-const { children } = useChildren();
+	const { status, data, error, close } = useEventSource('/sse', [], {
+		autoReconnect: {
+			retries: 3,
+			delay: 1000,
+			onFailed() {
+				alert('Failed to connect EventSource after 3 retries')
+			}
+		}
+	})
 
-const { data: load } = useFetch("/api/getAll");
-children.value = load;
+	children.value = JSON.parse(data.value)
 
-const { status, data, error, close } = useEventSource("/sse");
+	watch(status, () => {
+		// if (data.value === '') return
+		console.log(status.value)
+		// children.value = JSON.parse(data.value)
+	})
 
-watch(data, () => {
-  if (data.value === "") return;
-  children.value = JSON.parse(data.value);
-});
+	const carousel = useTemplateRef('carousel')
 
-const carousel = useTemplateRef("carousel");
+	function next() {
+		carousel.value.emblaApi.scrollNext()
+	}
 
-function next() {
-  carousel.value.emblaApi.scrollNext();
-}
+	function prev() {
+		carousel.value.emblaApi.scrollPrev()
+	}
 
-function prev() {
-  carousel.value.emblaApi.scrollPrev();
-}
+	let events
 
-onMounted(async () => {
-  onUnmounted(() => {
-    close();
-  });
-});
+	onMounted(async () => {
+		events = new EventSource('/sse')
 
-subscribeToSSE();
+		onUnmounted(() => {
+			console.log(status.value)
+			close()
+		})
+	})
 </script>
 
 <template>
-  <!-- <div class="actions pointer-events-none">
+	<!-- <div class="actions pointer-events-none">
     <UButton @click="prev" class="pointer-events-auto">Prev</UButton>
     <UButton @click="next" class="pointer-events-auto">Next</UButton>
   </div> -->
-  <section v-if="children" class="">
-    <NuxtLink
-      :to="`child/${child.id}`"
-      v-for="child in children"
-      :key="child.id"
-      >{{ child.firstName }}</NuxtLink
-    >
-    {{ c }}
-    <!-- <h2>Children</h2> -->
-    <!-- <ChildCard :key="children[0].id" :child="children[0]" /> -->
-    <!-- <UCarousel
-      v-slot="{ item }"
-      :items="children"
-      class="w-full max-w-xs mx-auto"
-    >
-      <ChildCard :child="item" />
-    </UCarousel> -->
-  </section>
+	<section v-if="children" class="">
+		<!-- <h2>Children</h2> -->
+		<!-- <ChildCard :key="children[0].id" :child="children[0]" /> -->
+		<UCarousel
+			v-slot="{ item }"
+			:items="children"
+			class="w-full max-w-xs mx-auto"
+		>
+			<ChildCard :child="item" />
+		</UCarousel>
+	</section>
 </template>
 
 <style scoped>
-.actions {
-  display: flex;
-  justify-content: space-between;
-  grid-column: 1 / -1;
-  grid-row: 1 / -1;
-  align-self: center;
-  z-index: 10;
-  padding-inline: 1rem;
-}
+	.actions {
+		display: flex;
+		justify-content: space-between;
+		grid-column: 1 / -1;
+		grid-row: 1 / -1;
+		align-self: center;
+		z-index: 10;
+		padding-inline: 1rem;
+	}
 
-.page {
-  grid-column: 1 / -1;
-  grid-row: 1 / -1;
-}
+	.page {
+		grid-column: 1 / -1;
+		grid-row: 1 / -1;
+	}
 </style>
